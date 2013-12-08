898dca73cdcc9e366c47b0e583081b7f17e1fe92
Shcsirf <Shcsirf@gmail.com>
2013-12-07 20:17:25 -0500

    final

--- a9/AVLremove.cpp
+++ a9/AVLremove.cpp
@@ -1,7 +1,7 @@
 // =============================================================================
 // AVLTree.cpp
 // ~~~~~~~~~~~
-// YOUR NAME
+// Sean Frischmann
 // implement the remove function of the AVL tree
 // =============================================================================
 
@@ -9,45 +9,12 @@
 #include <vector>
 #include <sstream>
 #include <stdexcept>
+#include "AVLTree.h"
 using namespace std; // BAD PRACTICE
 
 const string usage_msg = "AVL Tree Driver. Version 0.2\n"
                          "Author: Your Name\n"                       
                          "Report bugs to yourid@buffalo.edu";
-/**
- * -----------------------------------------------------------------------------
- * Maximum Method~
- *  Purpose is to find the right-most node of a binary search tree
- * -----------------------------------------------------------------------------
- **/
-template <typename Key>
-typename AVLTree<Key>::AVLNode* 
-maximum(typename AVLTree<Key>::AVLNode* root){
-	if(root == NULL){return NULL;}
-	while(root->right != NULL){
-		root = root->right;
-	}
-	return root;
-}
-/**
- * -----------------------------------------------------------------------------
- * Predecessor Method~
- *  Purpose is to find the right-most node on the left branch, otherwise the
- *  first ancestor whose right child is an ancestor
- * -----------------------------------------------------------------------------
- **/
-template <typename Key>
-typename AVLTree<Key>::AVLNode* 
-predecessor(typename AVLTree<Key>::AVLNode* node){
-	if(node == NULL){return NULL;}
-	if(node->left != NULL){return maximum(node->left);}
-	typename AVLTree<Key>::AVLNode* par = node->parent;
-	while((par != NULL) && (par->left == node)){
-		node = par;
-		par = par->parent;
-	}
-	return par;
-}
 /**
  * -----------------------------------------------------------------------------
  * Assignment 9: 
@@ -74,36 +41,43 @@ predecessor(typename AVLTree<Key>::AVLNode* node){
  */
 template <typename Key>
 bool AVLTree<Key>::remove(Key key) {
-	AVLNode* node_value = search(root_, key);
-	if(node_value == NULL){
+	AVLNode* node_to_delete = search(root_, key);
+	if(node_to_delete == NULL){
 		return false;
-	}else{
-		AVLNode* node_to_delete;
-		if((node_value->left == NULL) || (node_value->right == NULL)){
-				node_to_delete = node_value;
-		}else{
-			node_to_delete = predecessor(node_value);
-		}
-		AVLNode* node_child;
+	}
+	AVLNode* node_value;
+	if((node_to_delete->left == NULL) || (node_to_delete->right == NULL)){
 		if(node_to_delete->left == NULL){
-			node_child = node_to_delete->right;
+			node_value = node_to_delete->right;
 		}else{
-			node_child = node_to_delete->left;
+			node_value = node_to_delete->left;
 		}
-		AVLNode* node_par = node_to_delete->parent;
-		if(node_par == NULL){
-			root_ = NULL;
+	}else{
+		node_value = node_to_delete->left;
+		while(node_value->right != NULL){
+			node_value = node_value->right;
+		}
+		node_to_delete->key = node_value->key;
+		node_to_delete = node_value;
+		node_value = node_value->right;
+	}
+	AVLNode* node_par = node_to_delete->parent;
+	if(node_par == NULL){
+		root_ = node_value;
+	}else{
+		if(node_par->right == node_to_delete){
+			node_par->right = node_value;
 		}else{
-			if(node_par->left == node_to_delete){
-				node_par->left = node_child;
-			}else{
-				node_par->right = node_child;
-			}
+			node_par->left = node_value;
 		}
-		if(node_value != node_to_delete){
-			node_value->key = node_to_delete->key;
-		}
-		delete node_to_delete;
+	}
+	delete node_to_delete;
+	if(node_value != NULL){
+		node_value->parent = node_par;
+	}
+	if(node_par == NULL){
 		return true;
 	}
+	rebalance_after_insertion(root_);
+	return true;
 }
